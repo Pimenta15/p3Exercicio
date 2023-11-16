cf1432112a0f15557e2444a622cab55ceb1d7ee6
João Pedro Pimenta <125268689+Pimenta15@users.noreply.github.com>
2023-11-16 12:51:52 -0300

    prova3

---
+++ Coordenador.java
@@ -0,0 +1,24 @@
+public class Coordenador  extends Professor{
+    private String horarioDeAtendimento;
+
+    public Coordenador(String name, int idade, String curso, String matricula, String horarioDeAtendimento) {
+        super(name, idade, curso, matricula);
+        this.horarioDeAtendimento = horarioDeAtendimento;
+    }
+    
+    public void solicitacaoAluno(Aluno aluno){
+        aluno.setStatus(!aluno.isStatus());
+    }
+
+    public void removerTurma(Aluno aluno, Professor professor)throws ExepitionP3{
+    for (int i = 0; i < 19; i++) {
+        if (professor.getAluno()[i].getName() == aluno.getName() && professor.getAluno()[i].getMatricula() == aluno.getMatricula()){
+            professor.getAluno()[i] = null;
+        return;}  
+        }
+        throw new ExepitionP3("Aluno não encontrado");
+    }
+    
+    
+    }   
+
---
+++ ExepitionP3.java
@@ -0,0 +1,8 @@
+public class ExepitionP3 extends Exception{
+    
+
+    public ExepitionP3(String message) {
+        super(message);  
+    }
+    
+}
