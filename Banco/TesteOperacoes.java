6e9d774e29940bc3edc252a9f756ade9a70b5e41
João Pedro Pimenta <jpmp150404@gmail.com>
2023-11-28 20:55:17 -0300

    1.0

--- Banco/TesteOperacoes.java
+++ Banco/TesteOperacoes.java
@@ -5,13 +5,17 @@ import java.util.List;
 import java.util.Scanner;
 
 public class TesteOperacoes {
+    public static void main(String[] args) throws Exception {
+        apresentarMenu();
+    }
+
         
         static Scanner Scanner = new Scanner(System.in);
-        List<Cliente> listaClientes = new ArrayList<Cliente>();
-        List<Conta> listaContas = new ArrayList<Conta>();
-        int contas = 0;
+        static List<Cliente> listaClientes = new ArrayList<Cliente>();
+        static List<Conta> listaContas = new ArrayList<Conta>();
+       
     
-        public void criarConta(String nome, String endereco, String profissao,int conta,int agencia, String tipoConta){
+        public static  void criarConta(String nome, String endereco, String profissao,int conta,int agencia, String tipoConta){
             Conta c = new Conta(agencia, conta, null);
             if(!listaContas.contains(c)){
            Cliente cadastro = new Cliente(nome, endereco, profissao);
@@ -19,10 +23,10 @@ public class TesteOperacoes {
             tipoConta.toLowerCase();
             if (tipoConta.compareTo("poupança")==0){
                 
-                ContaPoupanca novConta = new ContaPoupanca(agencia, contas, cadastro);
+                ContaPoupanca novConta = new ContaPoupanca(agencia, conta, cadastro);
                 listaContas.add(novConta);
             }else if(tipoConta.compareTo("corrente")==0){
-                ContaCorrente novConta = new ContaCorrente(agencia, contas, cadastro);
+                ContaCorrente novConta = new ContaCorrente(agencia, conta, cadastro);
                 listaContas.add(novConta);
             }
         }else{
@@ -31,10 +35,30 @@ public class TesteOperacoes {
 
     }
 
-    public void realizarOperacoes(int agencia, int conta){
-
+    public static void realizarOperacoes(int agencia, int conta,int agencia2, int conta2) throws Exception{
+            int i;
+            Conta c, c2;
+            for(i=0;i<listaContas.size();i++){
+                if(listaContas.get(i).getConta()==conta &&listaContas.get(i).getAgencia()==agencia){
+                    c = listaContas.get(i);
+                for(i=0;i<listaContas.size();i++){
+                if(listaContas.get(i).getConta()==conta2 &&listaContas.get(i).getAgencia()==agencia2){
+                    c2 = listaContas.get(i);
+                    System.out.println("Digite o valor a ser transferido");
+                        double valor = Scanner.nextDouble();
+                        c.transferencia(c2, valor);
+                  
+                }else{
+                    System.out.println("Conta Destinataria não encontrado");
+                }
+                  
+                }
+                }else{
+                    System.out.println("Conta remetente não encontrado");
+                }
+            }
     }
-    public void exibirSaldo(int agencia,int conta){
+    public static void exibirSaldo(int agencia,int conta){
         int i;
         Conta c;
         for(i=0;i<listaContas.size();i++){
@@ -56,7 +80,7 @@ public class TesteOperacoes {
         }
 
     }
-         public void apresentarMenu() {
+         public static  void apresentarMenu() throws Exception {
         while (true) {
             System.out.println("Escolha uma opção:");
             System.out.println("1. Criar conta");
@@ -67,29 +91,34 @@ public class TesteOperacoes {
             int escolha = Scanner.nextInt();
             Scanner.nextLine(); 
             String nome, endereco, profissao, tipoConta;
-            int conta, agencia;
+            int conta, agencia, conta2, agencia2;
             switch (escolha) {
                 case 1:
                     System.out.println("Digite seu nome");
-                    nome = Scanner.nextLine();
+                    nome = Scanner.nextLine().toLowerCase();
                      System.out.println("Digite seu endereço");
-                    endereco = Scanner.nextLine();
+                    endereco = Scanner.nextLine().toLowerCase();
                     System.out.println("Digite sua profissao");
-                    profissao = Scanner.nextLine();
+                    profissao = Scanner.nextLine().toLowerCase();
                     System.out.println("Digite o número da conta");
                     conta = Scanner.nextInt();
                     System.out.println("Digite o número da agencia");
                     agencia = Scanner.nextInt();
+                    Scanner.nextLine();
                     System.out.println("Digite o tipo da conta");
-                    tipoConta =Scanner.nextLine();
+                    tipoConta =Scanner.nextLine().toLowerCase();
                     criarConta(nome,endereco,profissao,conta,agencia,tipoConta); 
                     break;
                 case 2:
-                    System.out.println("Digite o número da conta");
+                    System.out.println("Digite o número da sua conta");
                     conta = Scanner.nextInt();
-                    System.out.println("Digite o número da agencia");
+                    System.out.println("Digite o número da sua agencia");
                     agencia = Scanner.nextInt();
-                    realizarOperacoes(agencia,conta);
+                     System.out.println("Digite o número da conta destinataria");
+                    conta2 = Scanner.nextInt();
+                    System.out.println("Digite o número da agencia destinataria");
+                    agencia2 = Scanner.nextInt();
+                    realizarOperacoes(agencia,conta, agencia2, conta2);
                     break;
                 case 3:
                     System.out.println("Digite o número da conta");
@@ -106,5 +135,7 @@ public class TesteOperacoes {
             }
         }
     }
-    
 }
+    
+    
+    
